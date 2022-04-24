372a8f74bb7344a1ba7b7cacd4c9dfb7e065a019
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-04-24 14:14:02 -0300

    refactor(pastas): criado a pasta de Entidades e ViewModels
    Criado a pasta de Entidades, onde ficaram as Entidades que refletem as tabelas do Banco
    e a pasta de ViewModels para as requests e responses das controlers
    
    #9

---
+++ MVCAvisaEnchenteProject/Models/Entidades/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAvisaEnchenteProject.Models.Entidades
+{
+    public class BaseEntity
+    {
+        public virtual int Id { get; protected set; }
+    }
+}
---
+++ MVCAvisaEnchenteProject/Models/ViewModels/NotificacaoRiscoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAvisaEnchenteProject.Models.ViewModels
+{
+    public class NotificacaoRiscoModel
+    {
+        public string Titulo { get; set; }
+        public int TipoDoRisco { get; set; }
+        public string Link { get; set; }
+    }
+}
