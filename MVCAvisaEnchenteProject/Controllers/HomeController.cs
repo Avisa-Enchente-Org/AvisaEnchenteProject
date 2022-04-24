2967d56299f7a814bf3960774d047a3c33ffb2d9
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-04-24 14:18:22 -0300

    refactor(referencia): Ajutes de Referencia da pasta de ViewModels

--- MVCAvisaEnchenteProject/Controllers/HomeController.cs
+++ MVCAvisaEnchenteProject/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using MVCAvisaEnchenteProject.Models;
+using MVCAvisaEnchenteProject.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
