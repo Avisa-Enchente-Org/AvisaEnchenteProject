459457f644ba1ad109fefe4365159ea3692ab973
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-04-24 14:17:47 -0300

    feat(notificacao): Criada Partial View para notificações
    #13

--- MVCAvisaEnchenteProject/wwwroot/js/site.js
+++ MVCAvisaEnchenteProject/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
 
-// Write your JavaScript code.
+    if ($("div.alert.notification").length) {
+        setTimeout(() => {
+            $("div.alert.notification").fadeOut();
+        }, 4000);
+    }
+
+});
