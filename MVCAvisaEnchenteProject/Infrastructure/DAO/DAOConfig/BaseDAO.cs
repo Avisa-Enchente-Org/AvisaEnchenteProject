c9ecf43f6bb520fd4ad94665672d7510e5b69776
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-04-27 19:56:05 -0300

    feat(DAO): Criada BaseDAO Genérica
    Criada BaseDAO contendo os metodos padrões de CRUD
    
    #54

---
+++ MVCAvisaEnchenteProject/Infrastructure/DAO/DAOConfig/BaseDAO.cs
@@ -0,0 +1,78 @@
+﻿using MVCAvisaEnchenteProject.Models.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAvisaEnchenteProject.Infrastructure.DAO.DAOConfig
+{
+    public abstract class BaseDAO<T> where T : BaseEntity
+    {
+        public BaseDAO()
+        {
+            SetTabela();
+        }
+
+        protected string Tabela { get; set; }
+        protected string NomeSpListagem { get; set; } = "sp_Listagem";
+        protected abstract SqlParameter[] CriaParametros(T model);
+        protected abstract T MontaModel(DataRow registro);
+        protected abstract void SetTabela();
+
+        public virtual void Insert(T model)
+        {
+            HelperDAO.ExecutaProc("sp_Insert_" + Tabela, CriaParametros(model));
+        }
+
+        public virtual void Update(T model)
+        {
+            HelperDAO.ExecutaProc("sp_Update_" + Tabela, CriaParametros(model));
+        }
+
+        public virtual void Delete(int id)
+        {
+            var p = new SqlParameter[]
+            {
+                new SqlParameter("id", id),
+                new SqlParameter("tabela", Tabela)
+            };
+
+            HelperDAO.ExecutaProc("sp_Delete", p);
+        }
+
+        public virtual T Consulta(int id)
+        {
+            var p = new SqlParameter[]
+            {
+                new SqlParameter("id", id),
+                new SqlParameter("tabela", Tabela)
+            };
+
+            var tabela = HelperDAO.ExecutaProcSelect("sp_Consulta", p);
+
+            if (tabela.Rows.Count == 0)
+                return null;
+            else
+                return MontaModel(tabela.Rows[0]);
+        }
+
+        public virtual List<T> Listagem(int id)
+        {
+            var p = new SqlParameter[]
+            {
+                new SqlParameter("id", id),
+                new SqlParameter("tabela", Tabela)
+            };
+            var tabela = HelperDAO.ExecutaProcSelect(NomeSpListagem, p);
+
+            List<T> lista = new List<T>();
+
+            foreach (DataRow registro in tabela.Rows)
+                lista.Add(MontaModel(registro));
+
+            return lista;
+        }
+    }
+}
