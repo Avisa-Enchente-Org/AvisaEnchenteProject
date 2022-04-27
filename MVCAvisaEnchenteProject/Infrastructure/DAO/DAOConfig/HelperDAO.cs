5a51b42b26a0d7a8a9c8acb5c7321c3cba24f7c3
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-04-27 19:57:27 -0300

    feat(DAO): Criada a classe de HelperDAO e ConexaoBD
    Classe HelperDAO para executar as stored procedures e a ConexaoBD para se conectar ao banco
    
    #54

---
+++ MVCAvisaEnchenteProject/Infrastructure/DAO/DAOConfig/HelperDAO.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace MVCAvisaEnchenteProject.Infrastructure.DAO.DAOConfig
+{
+    public class HelperDAO
+    {
+        public static DataTable ExecutaProcSelect(string nomeProc, SqlParameter[] parametros)
+        {
+            using (SqlConnection conexao = ConexaoDB.GetConexao())
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(nomeProc, conexao))
+                {
+                    if (parametros != null)
+                        adapter.SelectCommand.Parameters.AddRange(parametros);
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    DataTable tabela = new DataTable();
+                    adapter.Fill(tabela);
+                    conexao.Close();
+                    return tabela;
+                }
+            }
+        }
+
+        public static void ExecutaProc(string nomeProc, SqlParameter[] parametros)
+        {
+            using (SqlConnection conexao = ConexaoDB.GetConexao())
+            {
+                using (SqlCommand comando = new SqlCommand(nomeProc, conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    if (parametros != null)
+                        comando.Parameters.AddRange(parametros);
+                    comando.ExecuteNonQuery();
+                }
+                conexao.Close();
+            }
+        }
+    }
+}
