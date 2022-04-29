78d099dbfc289d91eec8b9a1922ff2b846a01639
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-04-29 17:54:17 -0300

    feat(scripts): Adicionado o script para ativar as tabs atuais de cada pagina
    #10

---
+++ MVCAvisaEnchenteProject/wwwroot/js/pagina-inicial.js
@@ -0,0 +1,10 @@
+﻿
+window.addEventListener('load', function () {
+
+    $('#Index').addClass('active');
+    $('#SobreNos').removeClass('active');
+    $('#ComoFunciona').removeClass('active');
+    $('#MeuPerfil').removeClass('active');
+
+});
+
