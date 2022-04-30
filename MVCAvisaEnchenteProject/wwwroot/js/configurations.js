46f342e7e4a39d77de790f57d4ab4d23efaefab8
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-04-30 18:10:44 -0300

    refactor(Layout): Ajustes no layout e header

---
+++ MVCAvisaEnchenteProject/wwwroot/js/configurations.js
@@ -0,0 +1,11 @@
+﻿
+window.addEventListener('load', function () {
+
+    $('#index').removeClass('active');
+    $('#sobre-nos').removeClass('active');
+    $('#como-funciona').removeClass('active');
+    $('#meu-perfil-image').removeClass('active');
+    $('#configuracoes').addClass('active');
+
+});
+
--- MVCAvisaEnchenteProject/wwwroot/js/pagina-inicial.js
+++ MVCAvisaEnchenteProject/wwwroot/js/pagina-inicial.js
@@ -4,7 +4,8 @@ window.addEventListener('load', function () {
     $('#Index').addClass('active');
     $('#SobreNos').removeClass('active');
     $('#ComoFunciona').removeClass('active');
-    $('#MeuPerfil').removeClass('active');
+    $('#MeuPerfilImage').removeClass('active');
+    $('#Configuracoes').removeClass('active');
 
 });
 
