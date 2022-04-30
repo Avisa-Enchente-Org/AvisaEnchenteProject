7fa365d65ddabc293b025cb673613d74badef217
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-04-30 15:40:57 -0300

    feat(Enum): Criado classe de EnumHelper para obter o EnumDescription

---
+++ MVCAvisaEnchenteProject/Models/Enum/EnumHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAvisaEnchenteProject.Models.Enum
+{
+    public static class EnumHelper
+    {
+        public static string GetDescription<T>(this T enumValue)
+            where T : struct, IConvertible
+        {
+
+            if (!typeof(T).IsEnum)
+                return string.Empty;
+
+            var description = enumValue.ToString();
+            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+
+            if (fieldInfo != null)
+            {
+                var attrs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (attrs != null && attrs.Length > 0)
+                {
+                    description = ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+
+            return description ?? string.Empty;
+        }
+    }
+}
