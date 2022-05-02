2f8f76c001143f3802c0aa28f0569d18ce9eac66
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-05-02 01:19:37 -0300

    refactor(style): Atualizado o css e adicionado lib de table

--- MVCAvisaEnchenteProject/wwwroot/js/site.js
+++ MVCAvisaEnchenteProject/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿$(function () {
+﻿
+$(function () {
 
     if ($("div.notification").length) {
         setTimeout(() => {
@@ -7,3 +8,115 @@
     }
 
 });
+
+abrirFormModal = (url, titulo) => {
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (res) {
+            $('#form-modal .modal-body').html(res);
+            $('#form-modal .modal-title').html(titulo);
+            $('#form-modal').modal('show');
+
+        }
+    });
+}
+
+salvarUsuarioAjax = form => {
+
+    try {
+        $.ajax({
+            type: "POST",
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.erro) {
+                    Swal.fire({
+                        title: res.messageErro,
+                        confirmButtonColor: '#08357c',
+                        customClass: {
+                            confirmButton: 'btn btn-primary btn-md',
+                        },
+                    })
+                }
+                else {
+                    if (res.valido) {
+
+                        $('#form-modal .modal-body').html('');
+                        $('#form-modal .modal-title').html('');
+                        $('#form-modal').modal('hide');
+
+                        var pesquisaAvancadaUsuarios = document.getElementById('pesquisa-avancada-usuarios');
+                        return realizaPesquisaAvancadaUsuarios(pesquisaAvancadaUsuarios);
+    
+                    }
+                    else {
+                        $('#form-modal .modal-body').html(res.html);
+                    }
+                }
+
+              
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
+        return false;
+    } catch (e) {
+        console.log(e);
+    }
+    return false;
+}
+
+
+realizaPesquisaAvancadaUsuarios = form => {
+
+    try {
+        $.ajax({
+            type: "POST",
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (res) {
+
+                if (res.erro) {
+                    Swal.fire({
+                        title: res.messageErro,
+                        confirmButtonColor: '#08357c',
+                        customClass: {
+                            confirmButton: 'btn btn-primary btn-md',
+                        },
+                    })
+                }
+                else {
+                    $('#pv-listar-usuarios').html(res.html);
+                    rebuild();
+                }
+
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
+
+        return false;
+    } catch (e) {
+        console.log(e);
+    }
+
+    return false;
+}
+
+function loadingTemplate(message) {
+
+    return '<i class="fa fa-spinner fa-spin fa-fw fa-2x"></i>';
+}
+
+function rebuild() {
+    $('#table').bootstrapTable('destroy')
+        .bootstrapTable('showLoading')
+        .bootstrapTable()
+}
