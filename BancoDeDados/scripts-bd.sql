0b1ae34168ac27757d422e4ac2346e465da1010f
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-05-04 20:54:39 -0300

    feat(tabelas): criadas a tabelas de PDS, parametros de notificação, sensoriamento atual e historico de notificações

--- BancoDeDados/scripts-bd.sql
+++ BancoDeDados/scripts-bd.sql
@@ -10,22 +10,22 @@ USE [avisa_enchente_db]
 GO
 
 CREATE TABLE [dbo].[estados_atendidos](
-	id INT IDENTITY(1,1) NOT NULL,
-	descricao VARCHAR(max) NOT NULL,
-	uf VARCHAR(2) NOT NULL,
-	codigo_estado VARCHAR(2) NOT NULL,
+	[id] INT IDENTITY(1,1) NOT NULL,
+	[descricao] VARCHAR(max) NOT NULL,
+	[uf] VARCHAR(2) NOT NULL,
+	[codigo_estado] VARCHAR(2) NOT NULL,
 
 	CONSTRAINT [pk_estados_atendidos_id] PRIMARY KEY([id]),
 )
 GO
 
 CREATE TABLE [dbo].[cidades_atendidas](
-	id INT IDENTITY(1,1) NOT NULL,
-	descricao VARCHAR(max) NOT NULL,
-	codigo_cidade VARCHAR(20) NOT NULL,
-	estado_atendido_id INT NOT NULL,
-	latitude_ref DECIMAL(9,6) NOT NULL,
-	longitude_ref DECIMAL(9,6) NOT NULL,
+	[id] INT IDENTITY(1,1) NOT NULL,
+	[descricao] VARCHAR(max) NOT NULL,
+	[codigo_cidade] VARCHAR(20) NOT NULL,
+	[estado_atendido_id] INT NOT NULL,
+	[latitude_ref] DECIMAL(9,6) NOT NULL,
+	[longitude_ref] DECIMAL(9,6) NOT NULL,
 
 	CONSTRAINT [pk_cidades_atendidas_id] PRIMARY KEY([id]),
 	CONSTRAINT [fk_cidades_atendidas_estado_atendido_id] FOREIGN KEY([estado_atendido_id])
@@ -35,7 +35,7 @@ GO
  
 
 CREATE TABLE [dbo].[usuarios](
-	id INT IDENTITY(1,1) NOT NULL,
+	[id] INT IDENTITY(1,1) NOT NULL,
     [nome_completo] VARCHAR(100) NOT NULL,
 	[email] VARCHAR(200) NOT NULL,
 	[senha] VARCHAR(60) NOT NULL,	
@@ -46,6 +46,69 @@ CREATE TABLE [dbo].[usuarios](
 	CONSTRAINT [pk_usuarios_id] PRIMARY KEY([id]),
 	CONSTRAINT [fk_usuarios_cidade_atendida_id] FOREIGN KEY([cidade_atendida_id])
         REFERENCES [dbo].[cidades_atendidas]([id])
+)
+GO
+
+
+CREATE TABLE [dbo].[pontos_sensoriamento](
+	[id] INT IDENTITY(1,1) NOT NULL,
+    [descricao] VARCHAR(200) NOT NULL,
+	[ativo_helix] BIT NOT NULL,
+	[cidade_atendida_id] INT NULL,
+	[latitude] DECIMAL(9,6) NOT NULL,
+	[longitude] DECIMAL(9,6) NOT NULL,	
+	[usuario_id] INT NULL,
+
+	CONSTRAINT [pk_pontos_sensoriamento_id] PRIMARY KEY([id]),
+	CONSTRAINT [fk_pontos_sensoriamento_cidade_atendida_id] FOREIGN KEY([cidade_atendida_id])
+        REFERENCES [dbo].[cidades_atendidas]([id]),
+	CONSTRAINT [fk_pontos_sensoriamento_usuario_id] FOREIGN KEY([usuario_id])
+        REFERENCES [dbo].[usuarios]([id])
+)
+GO
+
+CREATE TABLE [dbo].[sensoriamento_atual](
+	[id] INT IDENTITY(1,1) NOT NULL,
+	[ponto_sensoriamento_id] INT NULL,
+	[nivel_pluviosidade] decimal(6,2) NOT NULL,
+	[vazao_agua] decimal(6,2) NOT NULL,
+	[altura_agua] decimal(6,2) NOT NULL,
+	[ultima_atualizacao] SMALLDATETIME NOT NULL DEFAULT GETDATE(),	
+	[tipo_risco] INT NOT NULL,
+
+	CONSTRAINT [pk_sensoriamento_atual_id] PRIMARY KEY([id]),
+	CONSTRAINT [fk_sensoriamento_atual_ponto_sensoriamento_id] FOREIGN KEY([ponto_sensoriamento_id])
+        REFERENCES [dbo].[pontos_sensoriamento]([id])
+)
+GO
+
+CREATE TABLE [dbo].[notificacoes_historico](
+	[id] INT IDENTITY(1,1) NOT NULL,
+	[ponto_sensoriamento_id] INT NULL,
+	[nivel_pluviosidade] decimal(6,2) NOT NULL,
+	[vazao_agua] decimal(6,2) NOT NULL,
+	[altura_agua] decimal(6,2) NOT NULL,
+	[data_notificacao] SMALLDATETIME NOT NULL DEFAULT GETDATE(),	
+	[tipo_risco] INT NOT NULL,
+
+	CONSTRAINT [pk_notificacoes_historico_id] PRIMARY KEY([id]),
+	CONSTRAINT [fk_notificacoes_historico_ponto_sensoriamento_id] FOREIGN KEY([ponto_sensoriamento_id])
+        REFERENCES [dbo].[pontos_sensoriamento]([id])
+)
+GO
+
+
+CREATE TABLE [dbo].[notificacoes_parametros](
+	[id] INT IDENTITY(1,1) NOT NULL,
+	[tipo_risco] INT NOT NULL,
+	[nivel_pluviosidade] decimal(6,2) NOT NULL,
+	[vazao_agua] decimal(6,2) NOT NULL,
+	[altura_agua] decimal(6,2) NOT NULL,
+	[ponto_sensoriamento_id] INT NULL,
+
+	CONSTRAINT [pk_notificacoes_parametros_id] PRIMARY KEY([id]),
+	CONSTRAINT [fk_notificacoes_parametros_ponto_sensoriamento_id] FOREIGN KEY([ponto_sensoriamento_id])
+        REFERENCES [dbo].[pontos_sensoriamento]([id])
 )
 GO
 
@@ -197,6 +260,19 @@ END
 GO
 
 
+CREATE PROCEDURE sp_atualiza_usuario_admin
+(
+	@id int,
+	@tipo_usuario INT
+)
+AS
+BEGIN
+	UPDATE usuarios SET
+	tipo_usuario = @tipo_usuario
+	WHERE id = @id
+END
+GO
+
 -- FUNCTIONS
 
 CREATE FUNCTION fnc_listar_usuarios()
@@ -214,4 +290,40 @@ CREATE VIEW vw_usuarios AS
 GO
 
 
-EXEC sp_listar 'usuarios'
+-- TRIGGERS
+
+CREATE TRIGGER trg_criaDependenciasDoPontoDeSensoriamento ON [dbo].[pontos_sensoriamento]
+FOR INSERT
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DECLARE @ponto_sensoriamento_id INT = (SELECT id FROM inserted)
+
+	INSERT INTO [dbo].[sensoriamento_atual] 
+	(ponto_sensoriamento_id, nivel_pluviosidade, vazao_agua, altura_agua, tipo_risco)
+	VALUES 
+	(@ponto_sensoriamento_id, 0.0, 0.0, 0.0, 0)
+
+	SET NOCOUNT OFF
+END
+GO
+
+CREATE TRIGGER trg_excluiDependenciasDoPontoDeSensoriamento ON [dbo].[pontos_sensoriamento]
+INSTEAD OF DELETE
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DECLARE @ponto_sensoriamento_id INT = (SELECT id FROM deleted)
+
+	DELETE FROM [dbo].[sensoriamento_atual]  
+		WHERE [ponto_sensoriamento_id] = @ponto_sensoriamento_id
+
+	DELETE FROM [dbo].[notificacoes_historico]  
+		WHERE [ponto_sensoriamento_id] = @ponto_sensoriamento_id
+
+	SET NOCOUNT OFF
+END
+GO
+
