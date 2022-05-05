813d79c0ac86b4806022556c6dc032d678573e87
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-05-04 21:20:04 -0300

    feat(ponto-de-sensoriamento): criada entidade de ponto de sensoriamento

---
+++ MVCAvisaEnchenteProject/Models/Entidades/PontoDeSensoriamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAvisaEnchenteProject.Models.Entidades
+{
+    public class PontoDeSensoriamento : BaseEntity
+    {
+        public string HelixId { get; set; }
+        public bool AtivoHelix { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public int CidadeAtendidaId { get; set; }
+        public int UsuarioId { get; set; }
+        public CidadeAtendida CidadeAtendida { get; set; }
+        public EstadoAtendido EstadoeAtendido { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
