181c5957c38d33396047a558d095ccc1b21b5c0c
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-05-04 22:27:01 -0300

    feat(PDS): Criado Controller de ponto de sensoriamento

---
+++ MVCAvisaEnchenteProject/Controllers/PontoDeSensoriamentoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MVCAvisaEnchenteProject.Infrastructure.DAO;
+using MVCAvisaEnchenteProject.Models.Entidades;
+using MVCAvisaEnchenteProject.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAvisaEnchenteProject.Controllers
+{
+    public class PontoDeSensoriamentoController : BaseController<PontoDeSensoriamento, PontoDeSensoriamentoDAO>
+    {
+        public PontoDeSensoriamentoController() : base()
+        {
+
+        }
+
+        [HttpGet]
+        [Authorize(Roles = nameof(ETipoUsuario.Admin))]
+        public override IActionResult Index()
+        {
+            return base.Index();
+        }
+
+    }
+}
