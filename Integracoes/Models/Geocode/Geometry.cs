1a7f0347430106cb16c77ce563204374096602fc
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2022-05-08 13:32:21 -0300

    refactor(localizacao): refatorado metodo de validar localização

---
+++ Integracoes/Models/Geocode/Geometry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Integracoes.Models.Geocode
+{
+    public class Geometry
+    {
+        public Location location { get; set; }
+    }
+}
