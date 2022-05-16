627ec6540905b355877fe9cebae1743bfd6ca51f
cavnicius <cayomagalhaes09@gmail.com>
2022-05-16 20:36:04 -0300

    feat(parametros-notificacao): criado dao de parametros notificacao

---
+++ MVCAvisaEnchenteProject/Infrastructure/DAO/ParametroNotificacaoDAO.cs
@@ -0,0 +1,41 @@
+﻿using MVCAvisaEnchenteProject.Infrastructure.DAO.DAOConfig;
+using MVCAvisaEnchenteProject.Models.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAvisaEnchenteProject.Infrastructure.DAO
+{
+    public class ParametroNotificacaoDAO : BaseDAO<ParametroNotificacao>
+    {
+        protected override void SetTabela()
+        {
+            Tabela = "notificacoes_parametros";
+        }
+
+        protected override SqlParameter[] CriaParametros(ParametroNotificacao parametroNotificacao)
+        {
+            var pontoDeSensoriamentoParametros = new List<SqlParameter>
+            {
+                new SqlParameter("ponto_sensoriamento_id", parametroNotificacao.PontoDeSensoriamentoId),
+                new SqlParameter("tipo_risco", parametroNotificacao.TipoRisco),
+                new SqlParameter("vazao_agua", parametroNotificacao.VazaoDaAgua),
+                new SqlParameter("nivel_pluviosidade", parametroNotificacao.NivelPluviosidade),
+                new SqlParameter("altura_agua", parametroNotificacao.AlturaAgua)
+            };
+            if (parametroNotificacao.Id > 0)
+                pontoDeSensoriamentoParametros.Add(new SqlParameter("id", parametroNotificacao.Id));
+
+            return pontoDeSensoriamentoParametros.ToArray();
+        }
+
+        protected override ParametroNotificacao MontaEntidadePadrao(DataRow registro)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ MVCAvisaEnchenteProject/Models/Entidades/NotificacaoHistorico.cs
@@ -0,0 +1,19 @@
+﻿using MVCAvisaEnchenteProject.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAvisaEnchenteProject.Models.Entidades
+{
+    public class NotificacaoHistorico : BaseEntity
+    {
+        public int PontoDeSensoriamentoId { get; set; }
+        public double NivelPluviosidade { get; set; }
+        public double VazaoDaAgua { get; set; }
+        public double AlturaAgua { get; set; }
+        public ETipoRisco TipoRisco { get; set; }
+        public DateTime DataNotificacao { get; set; }
+        public PontoDeSensoriamento PontoDeSensoriamento { get; set; }
+    }
+}
